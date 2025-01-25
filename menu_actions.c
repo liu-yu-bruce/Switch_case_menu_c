51179b2eac1783d7498d37d153bebaecec48cf7f
Bruce Liu <liubrucecomputers@gmail.com>
2025-01-25 12:11:12 -0700

    Add files via upload

---
+++ menu_actions.c
@@ -0,0 +1,14 @@
+void menu_action_1()
+{
+printf("input first action input in this function\n" );
+}
+
+void menu_action_2()
+{
+  printf("input second action input in this function here\n")
+}
+
+ bool menu_exit()
+{
+  return (0);
+}
