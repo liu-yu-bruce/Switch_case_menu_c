c2c3183f8bee2737bd19892b27dfc7feaa7c9d6a
Bruce Liu <liubrucecomputers@gmail.com>
2025-01-26 07:30:32 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,16 +1,12 @@
-//#include "menu_main.h"
-//#include "menu_selections.h"
-//#include "functions.h"
+
 
 #include <stdio.h>
-//void  hello_world();
+
 void menu_main();
-void menu_action_1();
-void menu_action_2();
-void menu_exit();
+
 int main()
 {
-//  hello_world();
+
   menu_main();
 return 0 ;
 }
