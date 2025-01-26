b990a3e09970792b9ff4856b1aa84642ed147b71
Bruce Liu <liubrucecomputers@gmail.com>
2025-01-26 07:25:33 -0700

    Add files via upload

--- menu_actions.c
+++ menu_actions.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+
+
 void menu_action_1()
 {
 printf("input first action input in this function\n" );
@@ -5,10 +8,10 @@ printf("input first action input in this function\n" );
 
 void menu_action_2()
 {
-  printf("input second action input in this function here\n")
+  printf("input second action input in this function here\n");
 }
 
- bool menu_exit()
+ int menu_exit()
 {
   return (0);
 }
